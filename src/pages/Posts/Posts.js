a2a95ac8cbf23af1cdff09e655b75f7119d7011b
Thallys Cezar <thalys_cezar@hotmail.com>
2022-08-16 17:33:35 -0300

    refactor: reconfig bug 2

--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -28,25 +28,26 @@ export default function Posts() {
     <div>
       <Navbar/>
         {posts.map(({ userId, id, title, body }) => (
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">User ID</th>
-                <th scope="col">ID</th>
-                <th scope="col">Title</th>
-                <th scope="col">Body</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>{userId}</td>
-                <td>{id}</td>
-                <td>{title}</td>
-                <td>{body}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="table-responsive">
+            <table className="table table-hove tabler-bordered w-100 p-3">
+              <thead>
+                <tr>
+                  <th scope="col">User</th>
+                  <th scope="col" key={id}>ID</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Body</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="text-center">{userId}</td>
+                  <td className="text-center">{id}</td>
+                  <td>{title}</td>
+                  <td>{body}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         ))}
     </div>
   );
